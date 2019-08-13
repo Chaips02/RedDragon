67cd4da99f07d06c5ef38babae0675a50afd9597
Chaips <chaips02@gmail.com>
2019-08-13 21:20:26 +0200

    - Import LeanTween library - Create levels logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject[] _levels;
+
+    // Level 1
+    public void OptionA()
     {
-        
+        _levels[1].SetActive(true);
+        LTDescr fadeOut = LeanTween.alphaCanvas(_levels[0].GetComponent<CanvasGroup>(), 0f, 2f);
+        fadeOut.setOnComplete(() => {
+            LTDescr fadeIn = LeanTween.alphaCanvas(_levels[1].GetComponent<CanvasGroup>(), 1f, 2f);
+            fadeIn.setOnComplete(() => {
+                _levels[0].SetActive(false);
+            });
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    // Level 1
+    public void OptionB()
     {
-        
+        _levels[2].SetActive(true);
+        LTDescr fadeOut = LeanTween.alphaCanvas(_levels[0].GetComponent<CanvasGroup>(), 0f, 2f);
+        fadeOut.setOnComplete(() => {
+            LTDescr fadeIn = LeanTween.alphaCanvas(_levels[2].GetComponent<CanvasGroup>(), 1f, 2f);
+            fadeIn.setOnComplete(() => {
+                _levels[0].SetActive(false);
+            });
+        });
     }
 }
